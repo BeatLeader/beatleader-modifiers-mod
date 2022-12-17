996a5ed8da424b9c33509af02e694e206cf479bf
NSGolova <golova@golova.dev>
2022-12-17 09:59:10 +0000

    Ver 2

--- BeatLeaderModifiers/HarmonyPatches/CutScoreBufferPatch.cs
+++ BeatLeaderModifiers/HarmonyPatches/CutScoreBufferPatch.cs
@@ -17,11 +17,13 @@ namespace BeatLeaderModifiers {
             int ____beforeCutScore,
             NoteCutInfo ____noteCutInfo) {
             if (StandardLevelBlaBlaPatch.customCharacterisitic == CustomCharacterisitic.betterScoring) {
-                int timingScore = Mathf.RoundToInt(Mathf.Max(0.1f - Mathf.Abs(____noteCutInfo.timeDeviation) - 0.02f, 0.0f) * 200);
-                int beforeCutScore = Mathf.RoundToInt(((float)____beforeCutScore / 30.0f) * 20);
-                int afterCutScore = Mathf.RoundToInt(((float)____afterCutScore / 70.0f) * 20f);
-                int centerDistanceCutScore = Mathf.RoundToInt(45f * (1f - Mathf.Clamp01(____noteCutInfo.cutDistanceToCenter / 0.3f)));
-                int angleScore = Mathf.RoundToInt((1f - Mathf.Min(Mathf.Abs(____noteCutInfo.cutAngle), 180f) / 180f) * 10f);
+                int timingScore = Mathf.RoundToInt(Mathf.Max(0.1f - (Mathf.Abs(____noteCutInfo.timeDeviation) - 0.02f), 0.0f) * 400);
+                int beforeCutScore = Mathf.RoundToInt(((float)____beforeCutScore / 70.0f) * 20);
+                int afterCutScore = Mathf.RoundToInt(((float)____afterCutScore / 30.0f) * 20f);
+                int centerDistanceCutScore = Mathf.RoundToInt(15f * (1f - Mathf.Clamp01(____noteCutInfo.cutDistanceToCenter / 0.3f)));
+                int angleScore = Mathf.RoundToInt(Mathf.Max(60f - Mathf.Abs(Mathf.Abs(____noteCutInfo.cutAngle) - 90f), 0f) / 3f);
+
+                Plugin.Log.Debug(timingScore + " " + beforeCutScore + " " + afterCutScore + " " + centerDistanceCutScore + " " + angleScore);
 
                 __result = timingScore + beforeCutScore + afterCutScore + centerDistanceCutScore + angleScore;
 
